e624be63aa4e13264e124f661bb36010e50d824b
John Matthews <johnmatthews2634@gmail.com>
2017-12-18 12:39:58 -0800

    add controllers and models

--- customer_info/config/routes.rb
+++ customer_info/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    resources :categories, only: [:create, :destroy, :show, :index]
+    resources :customers, only: [:create, :destroy, :show, :index]
+    resources :products, only: [:create, :destroy, :show, :index]
+    resources :order, only: [:create, :destroy, :show, :index]
+  end
 end

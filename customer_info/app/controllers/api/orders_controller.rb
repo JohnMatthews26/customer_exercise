6ac32100ddc791b1c2564b1ac93c65d971d82143
John Matthews <johnmatthews2634@gmail.com>
2017-12-21 20:24:38 -0800

    add search by customer

--- customer_info/app/controllers/api/orders_controller.rb
+++ customer_info/app/controllers/api/orders_controller.rb
@@ -1,19 +1,23 @@
 class Api::OrdersController < ApplicationController
   def index
-    p ["----", params[:start_date]["year"]]
-    p params[:start_date]["year"].to_i
-    p params[:end_date]
+
     date_range = (Date.new(params[:start_date]["year"].to_i,
     params[:start_date]["month"].to_i, params[:start_date]["day"].to_i)..Date.new(params[:end_date]["year"].to_i,
     params[:end_date]["month"].to_i, params[:end_date]["day"].to_i))
 
-    p date_range
     orders = Order.where(order_date: date_range)
-    render(
-    json: [orders]
-    )
+    @product_orders = []
+    orders.each do |order|
+      single_order = ProductOrder.where(order_id: order.id)
+      @product_orders.push({customer_id: order.customer_id,
+        order_date: order.order_date,
+        product_id: single_order[0].product_id,
+        number_purchased: single_order[0].number_purchased})
+    end
   end
 
+
+  private
   def order_params
     params.require(:order).permit(:start_date, :end_date)
   end
--- customer_info/db/seeds.rb
+++ customer_info/db/seeds.rb
@@ -6,5 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'date'
+Customer.destroy_all
+Product.destroy_all
+Order.destroy_all
+ProductOrder.destroy_all
+
 customer1 = Customer.create(customer_first_name: "John")
+product1 = Product.create(product_name: "Shoes")
 order1 = Order.create(customer_id: customer1.id, order_status: "delivered", order_date: Date.today)
+productorder1 = ProductOrder.create(product_id: product1.id, number_purchased: 15, order_id: order1.id)

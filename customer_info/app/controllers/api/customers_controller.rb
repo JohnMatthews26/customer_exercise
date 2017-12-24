68b9ae412d4cdbeb902d8dba602fc09ca3a16fb3
John Matthews <johnmatthews2634@gmail.com>
2017-12-24 10:45:22 -0800

    modify orders api

--- customer_info/app/controllers/api/customers_controller.rb
+++ customer_info/app/controllers/api/customers_controller.rb
@@ -14,6 +14,21 @@ class Api::CustomersController < ApplicationController
 
   end
 
+  def show
+    @customer_orders = Order.where(customer_id: params[:customer_id])
+
+    @product_orders = []
+    @customer_orders.each do |order|
+      single_order = ProductOrder.where(order_id: order.id)
+      @product_orders.push({customer_id: order.customer_id,
+        order_date: order.order_date,
+        product_id: single_order[0].product_id,
+        number_purchased: single_order[0].number_purchased})
+    end
+    render json: @product_orders
+  end
+
+
   private
   def order_params
     params.require(:customer).permit(:customer_id)
--- customer_info/app/controllers/api/orders_controller.rb
+++ customer_info/app/controllers/api/orders_controller.rb
@@ -8,10 +8,11 @@ class Api::OrdersController < ApplicationController
     @product_orders = []
     orders.each do |order|
       single_order = ProductOrder.where(order_id: order.id)
-      @product_orders.push({customer_id: order.customer_id,
+      @product_orders.push({
         order_date: order.order_date,
         product_id: single_order[0].product_id,
-        number_purchased: single_order[0].number_purchased})
+        number_purchased: single_order[0].number_purchased
+        })
     end
     render json: @product_orders
 
